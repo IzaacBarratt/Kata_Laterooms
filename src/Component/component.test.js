17558343ac24889a5f0c15e3167108b7a433c81e
Izaac Barratt <izaacjb@hotmail.co.uk>
2019-05-03 04:25:19 +0100

    tests added based on project plan/structure

---
+++ src/Component/component.test.js
@@ -0,0 +1,15 @@
+test('Searched items show up on the page', () => {
+    // write test here
+})
+
+test('Changing filter reflects in redux cache', () => {
+
+})
+
+test('Filter function removes items that aren\'t queried', () => {
+
+})
+
+
+
+
